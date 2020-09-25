5554633beab6a225a32b7d66dda25a62f33be52b
acydcross <acydcross@hotmail.com>
2020-09-24 20:33:56 -0600

    Created a clickable to get a random joke

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-
+    <joke-button></joke-button>
   </div>
 </template>
 
 <script>
-import JokeButton from './components/jokebutton.vue'
-import LoudJoke from './components/loadjoke.vue'
-import NormalJoke from './components/loadjoke.vue'
-import SnakeJoke from './components/snakejoke.vue'
+import JokeButton from "./components/jokebutton.vue";
+// import LoudJoke from './components/loadjoke.vue'
+// import NormalJoke from './components/loadjoke.vue'
+// import SnakeJoke from './components/snakejoke.vue'
 
 export default {
   name: "App",
   components: {
-    JokeButton,
-    LoudJoke,
-    NormalJoke,
-    SnakeJoke
+    JokeButton
+    // LoudJoke,
+    // NormalJoke,
+    // SnakeJoke
 
   }
 };
--- src/components/jokebutton.vue
+++ src/components/jokebutton.vue
@@ -1,12 +1,31 @@
 <template>
     <div>
-
+        <h2 @click="getJoke">{{ joke }}</h2>
     </div>
 </template>
 
 <script>
+import axios from "axios"
+
     export default {
-        
+        name: "geek-joke",
+        data() {
+            return {
+                joke: "Click Me!"
+            }
+        },
+        methods: {
+            getJoke: function() {
+                axios.request( {
+                    url: "https://geek-jokes.sameerkumar.website/api?format=json",
+                    method: "GET"
+                }).then((response) => {
+                    this.joke = response.data.joke
+                }).catch((error) => {
+                    console.log(error)
+                })
+            }
+        },
     }
 </script>
 
